7bc97c53a81341b163688766d076074ef76a5089
Sebastian Scherbes <github@scherbes.de>
2022-09-08 16:10:43 +0200

    add create-quiz-functionality

--- components/form/form.js
+++ components/form/form.js
@@ -1,3 +1,4 @@
+const cardList = document.querySelector("[data-js='card-list']");
 const cardForm = document.querySelector("[data-js='card-form']");
 const questionText = document.querySelector("[data-js='question-text']");
 const answerText = document.querySelector("[data-js='answer-text']");
@@ -13,6 +14,46 @@ answerText.addEventListener("input", countCharacters);
 
 cardForm.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  const cardBox = document.createElement("li");
+  cardBox.classList = "card__item";
+  cardList.append(cardBox);
+
+  const bookmarkButton = document.createElement("button");
+  bookmarkButton.classList = "card__bookmark-button";
+  cardBox.append(bookmarkButton);
+
+  const bookmarkIcon = document.createElement("img");
+  bookmarkIcon.src = "img/bookmark_add.svg";
+  bookmarkIcon.alt = "Add to bookmarks";
+  bookmarkIcon.classList = "card__bookmark-icon";
+  bookmarkButton.append(bookmarkIcon);
+
+  const cardQuestion = document.createElement("p");
+  cardQuestion.classList = "card__question";
+  cardQuestion.textContent = event.target.question.value;
+  cardBox.append(cardQuestion);
+
+  const revealButton = document.createElement("button");
+  revealButton.classList = "card__reveal-button";
+  revealButton.dataset.js = "reveal-button";
+  revealButton.textContent = "Show answer";
+  cardBox.append(revealButton);
+
+  const cardAnswer = document.createElement("p");
+  cardAnswer.classList = "card__answer";
+  cardAnswer.dataset.js = "answer";
+  cardAnswer.textContent = event.target.answer.value;
+  cardBox.append(cardAnswer);
+
+  const tagList = document.createElement("ul");
+  tagList.classList = "card__tag-list";
+  cardBox.append(tagList);
+
+  const tagListItem = document.createElement("li");
+  tagListItem.classList = "card__tag";
+  tagListItem.textContent = event.target.tag.value;
+  tagList.append(tagListItem);
 });
 
 function countCharacters(event) {
@@ -20,16 +61,16 @@ function countCharacters(event) {
   if (event.target.dataset.js === "question-text") {
     questionCounter.textContent = `${counter} characters left`;
     if (counter === 0) {
-      questionCounter.classList.add("card-form__counter--red");
+      questionCounter.classList.add("card__form__counter--red");
     } else {
-      questionCounter.classList.remove("card-form__counter--red");
+      questionCounter.classList.remove("card__form__counter--red");
     }
   } else {
     answerCounter.textContent = `${counter} characters left`;
     if (counter === 0) {
-      answerCounter.classList.add("card-form__counter--red");
+      answerCounter.classList.add("card__form__counter--red");
     } else {
-      answerCounter.classList.remove("card-form__counter--red");
+      answerCounter.classList.remove("card__form__counter--red");
     }
   }
 }
